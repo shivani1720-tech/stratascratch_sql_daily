77388206834bb839b075e3ddf900d30e4197298d
shivani1720-tech <shivani.sharma17032000@gmail.com>
2025-01-05 19:53:13 -0500

    Update ID_10539 User Activity Count.sql

--- Easy/ID_10539 User Activity Count.sql
+++ Easy/ID_10539 User Activity Count.sql
@@ -1,1 +1,25 @@
+-- User Activity Count
+-- Interview Question Date: October 2024
+/*
+Question
+Count the unique activity types for each user, ensuring users with no activities are also included.
+The output should show each user's ID and their activity type count, with zero for users who have no activities.
 
+Tables: user_profiles, activity_log
+user_profiles table
+  email: text
+  name: text
+  signup_date: date
+  user_id: bigint
+
+activity_log table
+  activity_timestamp:date
+  activity_type:text
+  user_id:bigint
+*/
+--solution
+
+SELECT up.user_id, COUNT(al.activity_type) as activity_type_count
+FROM USER_PROFILES up
+JOIN ACTIVITY_LOG al ON al.user_id = up.user_id
+GROUP BY up.user_id
